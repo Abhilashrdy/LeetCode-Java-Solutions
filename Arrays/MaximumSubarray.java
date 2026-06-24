177bd42cadb924b4ae2b7e19dc5ec70ac6ebeba6
B. Abhilash Reddy <abhilashrdy.b@gmail.com>
2026-06-24 09:36:09 +0530

    Update MaximumSubarray.java

--- Arrays/MaximumSubarray.java
+++ Arrays/MaximumSubarray.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxSubArray(int[] nums) {
+      int  sum = -30000,max = Integer.MIN_VALUE,n = nums.length;
+    for(int i = 0 ; i < n ; i++){
+        if(sum+nums[i] < nums[i] || sum + nums[i] <= 0){
+            sum = nums[i];
+        }
+        else{
+            sum += nums[i];
+        }
+        max = Math.max(max,sum);
+    }
+    return max;
+    }
+}
