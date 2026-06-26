4a77d08bbab2804c3dad12f0b244a280195dc2b2
B. Abhilash Reddy <abhilashrdy.b@gmail.com>
2026-06-26 18:35:16 +0530

    Array Solution Added

---
+++ Arrays/Leetcode 566. Reshape the Matrix.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+         if (mat.length * mat[0].length != r * c)
+            return mat;
+        int[][] result = new int[r][c];
+        for(int i = 0; i < r * c; i++)
+            result[i / c][i % c] = mat[i / mat[0].length][i % mat[0].length];
+        return result;
+    }
+}
