c74dce3fce692a118b1fd27b6b466104ab8ca0ea
B. Abhilash Reddy <abhilashrdy.b@gmail.com>
2026-06-27 08:41:27 +0530

    Binary Search Solution

---
+++ BinarySearch/0744_FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        if(letters[letters.length-1] <= target) return letters[0];
+        int start = 0 , end = letters.length;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(letters[mid] > target){
+                end = mid-1;
+            }
+            else {
+                start = mid+ 1;
+            }
+        }
+        return letters[start];
+    }
+}
